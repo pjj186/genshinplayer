88bd13cbb77bdcd6441cae0646ae0b584418eeea
Jong jae Park <pjj186@naver.com>
2021-10-28 11:29:45 +0900

    PlayButton 컴포넌트 UI

---
+++ src/components/PlayButton.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
+
+const PlayButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  font-size: 25px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 1); // 그림자 나중에 수정
+  cursor: pointer;
+`;
+
+const PlayButton = () => {
+  const [isPlay, setIsPlay] = useState(false);
+
+  const onClickPlay = () => {
+    setIsPlay((prev) => !prev);
+  };
+
+  return (
+    <PlayButtonContainer onClick={onClickPlay}>
+      {isPlay ? (
+        <FontAwesomeIcon icon={faPlay} />
+      ) : (
+        <FontAwesomeIcon icon={faPause} />
+      )}
+    </PlayButtonContainer>
+  );
+};
+
+export default PlayButton;
