3a112cb5dcdec81ac45cad3c7df3fcdbeedbc8b8
Jong jae Park <pjj186@naver.com>
2021-10-30 17:06:30 +0900

    리스트 박스 만들기

--- src/routes/MusicList.js
+++ src/routes/MusicList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Header from "../components/common/Header";
 
 const Container = styled.div`
   display: flex;
@@ -16,10 +17,43 @@ const Player = styled.div`
   border: 1px solid black;
 `;
 
+const Title = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  font-weight: bold;
+`;
+
+const ListBox = styled.div`
+  margin: 0 auto;
+  margin-top: 20px;
+  width: 90%;
+  height: 400px;
+  border: 1px solid black;
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    height: 8px;
+    width: 5px;
+    border-radius: 6px;
+    background: rgba(0, 0, 0, 0.8);
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(255, 255, 255, 0.4);
+    border-radius: 6px;
+  }
+`;
+
 const MusicList = () => {
   return (
     <Container>
-      <Player />
+      <Player>
+        <Header>
+          <Title>
+            <span>Genshin Music</span>
+          </Title>
+        </Header>
+        <ListBox></ListBox>
+      </Player>
     </Container>
   );
 };
