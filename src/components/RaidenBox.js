fc9846f86893f6118872f3ff0c61ab33f08360ab
Jong jae Park <pjj186@naver.com>
2021-11-01 13:08:25 +0900

    RaidenBox Add Link

--- src/components/RaidenBox.js
+++ src/components/RaidenBox.js
@@ -2,12 +2,34 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { storageService } from "../fbase";
 import { getDownloadURL, ref } from "@firebase/storage";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlayCircle } from "@fortawesome/free-regular-svg-icons";
 
 const Container = styled.div`
   display: flex;
   width: 100%;
   height: 60px;
   border: 1px solid black;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const TextBox = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  margin-left: 10px;
+  cursor: default;
+`;
+
+const AvatarImg = styled.img`
+  height: 55px;
+  width: 55px;
+`;
+
+const Icon = styled.div`
+  font-size: 40px;
+  margin-right: 10px;
 `;
 
 const RaidenBox = () => {
@@ -31,7 +53,13 @@ const RaidenBox = () => {
 
   return (
     <Container>
-      <img src={imgSrc} alt="Avatar"></img>
+      <AvatarImg src={imgSrc} alt="Avatar" />
+      <TextBox>Raiden Shogun</TextBox>
+      <Link to="/play">
+        <Icon>
+          <FontAwesomeIcon icon={faPlayCircle} />
+        </Icon>
+      </Link>
     </Container>
   );
 };
