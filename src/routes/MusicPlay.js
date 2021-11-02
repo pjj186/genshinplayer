adee8ba1815da79cbdf9f6c2357c293be6d121c6
Jong jae Park <pjj186@naver.com>
2021-11-02 15:58:11 +0900

    타임라인 포매팅, 타임라인 max 설정

--- src/routes/MusicPlay.js
+++ src/routes/MusicPlay.js
@@ -75,14 +75,16 @@ const MusicPlay = (props) => {
   const [srcValue, setSrcValue] = useState("");
   const [volumeBar, setVolumeBar] = useState(0.5);
   const [timeline, setTimeline] = useState(0);
-  const [duration, setDuration] = useState(0);
+  const [duration, setDuration] = useState(null);
+  const [originduration, setOriginDuration] = useState(null);
 
   const getLocalForage = () => {
     localforage.getItem(MUSIC_LF, (err, value) => {
       if (err) {
         console.log(err);
       }
-      setDuration(value.duration);
+      setDuration(timeFormat(value.duration));
+      setOriginDuration(value.duration);
     });
   };
 
@@ -108,7 +110,9 @@ const MusicPlay = (props) => {
   };
 
   const handleTimeUpdate = () => {
-    currentTime.current.innerText = Math.floor(videoRef.current.currentTime);
+    currentTime.current.innerText = timeFormat(
+      Math.floor(videoRef.current.currentTime)
+    );
     setTimeline(Math.floor(videoRef.current.currentTime));
   };
 
@@ -119,13 +123,20 @@ const MusicPlay = (props) => {
     videoRef.current.currentTime = value;
   };
 
+  const timeFormat = (seconds) => {
+    // 시간 형식을 포맷하는 함수
+    return new Date(seconds * 1000).toISOString().substr(15, 4);
+  };
+
   useEffect(() => {
     const video = videoRef.current;
-    volumeRef.current.addEventListener("input", handleVolumeChange);
+    const volume = volumeRef.current;
+    volume.addEventListener("input", handleVolumeChange);
     video.addEventListener("timeupdate", handleTimeUpdate);
     getLocalForage();
     return () => {
       video.removeEventListener("timeupdate", handleTimeUpdate);
+      volume.removeEventListener("input", handleVolumeChange);
     };
   }, []);
 
@@ -158,13 +169,12 @@ const MusicPlay = (props) => {
               step="1"
               value={timeline}
               min="0"
+              max={originduration}
               ref={timelineel}
               onChange={handleTimelineChange}
             />
-            <CurrentTimeBox ref={currentTime}>00 : 00</CurrentTimeBox>
-            <TotalTimeBox ref={totalTime}>
-              {Math.floor(duration / 60)} : {Math.floor(duration % 60)}
-            </TotalTimeBox>
+            <CurrentTimeBox ref={currentTime}>0:00</CurrentTimeBox>
+            <TotalTimeBox ref={totalTime}>{duration}</TotalTimeBox>
             <Video src={srcValue} autoPlay={true} ref={videoRef} />
           </SongController>
           <ButtonContainer>
