83bf3ada99d97167033b1d0abed2b65c1f7b8205
Jong jae Park <pjj186@naver.com>
2022-01-07 10:16:14 +0900

    주석 지우기

--- src/routes/MusicPlay.js
+++ src/routes/MusicPlay.js
@@ -184,11 +184,7 @@ const MusicPlay = ({ location }) => {
 
   const loading = LFContext.musicLF || LFContext.imageLF || LFContext.bgLF;
 
-  // console.log(localkeys);
   let volumeValue = volumeBar; // 볼륨 초깃값
-
-  // 재생 페이지에서 새로 고침 시, 다시 데이터를 불러오고, 로딩을 끝내줄 함수가 필요함
-  // duration, originDuration, name, img, bg 정보 다시 불러와야함
 
   // 음악 재생
   const playMusic = () => {
@@ -273,10 +269,8 @@ const MusicPlay = ({ location }) => {
         LFContext.setBgLF(false);
       });
   };
-  // 새로고침 했을 때, localforage에 있는 데이터들을 Context State에 다시 저장하는 함수를 만들어보자
+
   const reloadData = async () => {
-    // 1. 우선, 로컬 포리지에 값이 저장돼있을때만 작동하게 만들어야함. (로컬 포리지 DOCS를 참고해서 쓸수 있는 메서드가 있는지 알아보자.)
-    // 2. 그 다음은 그냥 getitem 해서 Context State에 값만 넣어주면 될것같음! * 캐릭터 박스에 있는 로직을 쓰면 될것같다.
     await localforage.keys().then((res) => {
       if (res.length === 3) {
         getMusicInfo();
